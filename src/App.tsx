a4b51a686496efa2ae7fc3589fcb2d2eb7d6cfbd
Ganna Demchuk <anyutkadem@gmail.com>
2024-03-09 22:23:43 +0100

    add navbar and login register buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
 import React from "react";
-import LoginForm from "./components/LoginForm";
+import Navbar from "./components/Navbar";
 
 function App() {
-    return (
-        <div className="w-full h-screen flex items-center justify-center">
-            <div className="text-center">
-                <h1 className="text-[56px] text-red-600">
-                    Welcome to Loquela!
-                </h1>
-                <h4 className="text-2xl">
-                    Enjoy your best learning web application.
-                </h4>
-                <div className="mt-20 pl-20">
-                    <LoginForm />
-                </div>
-            </div>
-        </div>
-    );
+    return <Navbar />;
 }
 
 export default App;
