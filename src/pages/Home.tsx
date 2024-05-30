98ed62d1358fb16ac869c565bc6b5f7877c080fb
Ganna Demchuk <anyutkadem@gmail.com>
2024-05-30 22:48:04 +0200

    home page translation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,13 +9,14 @@ import { FaRegStar } from "react-icons/fa";
 import { IoInfiniteOutline } from "react-icons/io5";
 import { GiDiamondHard } from "react-icons/gi";
 import { BiError } from "react-icons/bi";
+import { useTranslation } from "react-i18next";
 
 type RecommendationsCard = {
     icon: JSX.Element;
     description: string;
 };
 
-const recommendationsCards: RecommendationsCard[] = [
+const recommendationsCardsEN: RecommendationsCard[] = [
     {
         icon: <IoInfiniteOutline size={36} color="balck" />,
         description: "Practice consistently every day, even briefly, as regularity is crucial for steady progress.",
@@ -31,8 +32,68 @@ const recommendationsCards: RecommendationsCard[] = [
     },
 ];
 
+const recommendationsCardsUA: RecommendationsCard[] = [
+    {
+        icon: <IoInfiniteOutline size={36} color="black" />,
+        description:
+            "Практикуйтеся регулярно кожен день, навіть коротко, оскільки регулярність є ключовою для стабільного прогресу.",
+    },
+    {
+        icon: <GiDiamondHard size={36} color="black" />,
+        description: "Викликайте себе, виконуючи більш складні завдання та тести за межами своєї зони комфорту.",
+    },
+    {
+        icon: <BiError size={36} color="black" />,
+        description:
+            "Прийміть, що помилки є природною частиною навчання; ретельно аналізуйте та вчіться з кожної помилки.",
+    },
+];
+
+const recommendationsCardsDE: RecommendationsCard[] = [
+    {
+        icon: <IoInfiniteOutline size={36} color="black" />,
+        description:
+            "Üben Sie jeden Tag konsequent, auch wenn es nur kurz ist, da Regelmäßigkeit entscheidend für stetigen Fortschritt ist.",
+    },
+    {
+        icon: <GiDiamondHard size={36} color="black" />,
+        description:
+            "Fordern Sie sich heraus, indem Sie sich auf anspruchsvollere Aufgaben und Tests außerhalb Ihrer Komfortzone einlassen.",
+    },
+    {
+        icon: <BiError size={36} color="black" />,
+        description:
+            "Akzeptieren Sie, dass Fehler ein natürlicher Teil des Lernens sind; analysieren Sie jeden Fehler sorgfältig und lernen Sie daraus.",
+    },
+];
+
+const languageTranslations: Record<string, Record<string, string>> = {
+    English: {
+        en: "English",
+        ua: "Англійська",
+        de: "Englisch",
+    },
+    Ukrainian: {
+        en: "Ukrainian",
+        ua: "Українська",
+        de: "Ukrainisch",
+    },
+    German: {
+        en: "German",
+        ua: "Німецька",
+        de: "Deutsch",
+    },
+};
+
+const recommendationsCardsMap: Record<string, RecommendationsCard[]> = {
+    en: recommendationsCardsEN,
+    ua: recommendationsCardsUA,
+    de: recommendationsCardsDE,
+};
+
 const Home = () => {
     const { userData } = useUser();
+    const { t, i18n } = useTranslation();
 
     if (!userData) {
         return (
@@ -41,6 +102,8 @@ const Home = () => {
             </div>
         );
     }
+
+    const recommendationsCards = recommendationsCardsMap[i18n.language];
 
     const getCurrentLanguageLevel = () => {
         switch (userData.users.learnLanguage) {
@@ -55,30 +118,36 @@ const Home = () => {
         }
     };
 
+    const getTranslatedLanguage = (language: string, currentLang: string) => {
+        return languageTranslations[language] ? languageTranslations[language][currentLang] : language;
+    };
+
     return (
         <>
             <SideBar>
                 <div className="flex flex-col space-y-12">
                     <div className="mt-4 text-center">
                         <h1 className=" space-y-4 text-[40px] font-semibold italic tracking-tight leading-none">
-                            Hello, <span className="text-cyan-600">{userData.users.fullName}</span>. Welcome to Loquela!
+                            {t("home.greeting")}
+                            <span className="text-cyan-600">{userData.users.fullName}</span>
+                            {t("home.welcome")}
                         </h1>
                     </div>
                     <div className="mt-10 grid grid-cols-1 lg:grid-cols-2 xxl:grid-cols-3 gap-x-6 gap-y-10">
                         <Card className="border-2 hover:border-2 hover:border-cyan-600">
                             <div className="flex flex-row justify-between">
-                                <h5 className="text-xl font-normal text-gray-500">Selected Language</h5>
+                                <h5 className="text-xl font-normal text-gray-500">{t("home.selected")}</h5>
                                 <TbLanguage size={32} className="text-gray-600" />
                             </div>
                             <div className="flex flex-row justify-center gap-2">
                                 <div className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    {userData.users.learnLanguage}
+                                    {getTranslatedLanguage(userData.users.learnLanguage, i18n.language)}
                                 </div>
                             </div>
                         </Card>
                         <Card className="border-2 hover:border-2 hover:border-cyan-600">
                             <div className="flex flex-row justify-between">
-                                <h5 className="text-xl font-normal text-gray-500">Language Level</h5>
+                                <h5 className="text-xl font-normal text-gray-500">{t("home.level")}</h5>
                                 <FaRegStar size={32} className="text-gray-600" />
                             </div>
                             <div className="flex flex-row justify-center gap-2">
@@ -89,14 +158,14 @@ const Home = () => {
                         </Card>
                         <Card className="border-2 hover:border-2 hover:border-cyan-600">
                             <div className="flex flex-row justify-between">
-                                <h5 className="text-xl font-normal text-gray-500">Daily Streak</h5>
+                                <h5 className="text-xl font-normal text-gray-500">{t("home.streak")}</h5>
                                 <GoTrophy size={32} className="text-gray-600" />
                             </div>
                             <div className="flex flex-row justify-center gap-2">
                                 <div className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
                                     {userData.user_practice_stats.daily_streak}
                                 </div>
-                                <div className="text-xl pt-2.5 font-normal text-gray-500">days</div>
+                                <div className="text-xl pt-2.5 font-normal text-gray-500">{t("home.days")}</div>
                             </div>
                         </Card>
                     </div>
@@ -124,13 +193,12 @@ const Home = () => {
                             </svg>
                             <Blockquote>
                                 <p className="text-2xl font-medium italic text-gray-900 dark:text-white">
-                                    “Learning another language is not only learning different words for the same things,
-                                    but learning another way to think about things.”{" "}
+                                    {t("home.quote")}
                                 </p>
                             </Blockquote>
                             <figcaption className="mt-6 flex items-center justify-center space-x-3">
                                 <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-                                    <cite className="pr-3 font-medium text-gray-900">Flora Lewis</cite>
+                                    <cite className="pr-3 font-medium text-gray-900">{t("home.author")}</cite>
                                 </div>
                             </figcaption>
                         </figure>
